480e29b6e663d9ed486ef03a2704a4c9f37ee508
Piotr Suwała <psuwala@atlassian.com>
2020-08-04 22:06:30 +0200

    Added viewing conditions for outputs.

--- front/src/frontend/view/register/dynamo/views/output/Output.tsx
+++ front/src/frontend/view/register/dynamo/views/output/Output.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+export function isDefined(str: string) {
+    return str !== undefined;
+}
+
 export function Output(props: RenderProps) {
-    return props.value !== undefined ? <div>{props.children(props.value)}</div> : null;
+    return (props.shouldDisplay || isDefined)(props.value) ? (
+        <div>{props.children(props.value)}</div>
+    ) : null;
 }
 
 export class RenderProps {
-    constructor(public value: string, public children: (value: string) => string) {}
+    constructor(
+        public value: string,
+        public children: (value: string) => string,
+        public shouldDisplay?: (value: string) => boolean
+    ) {}
 }
